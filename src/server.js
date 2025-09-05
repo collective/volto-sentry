c1f8c4ba2466e07898e1d06b4072a8c5fbe3d7fc
Dobricean Ioan Dorian <50819975+dobri1408@users.noreply.github.com>
2025-09-05 09:04:24 +0300

    ssr error

--- src/server.js
+++ src/server.js
@@ -6,3 +6,20 @@ import initSentry from './sentry';
 export default function apply() {
   initSentry({ Sentry, SentryIntegrations });
 }
+
+export const captureSSRException = (error, context = {}) => {
+  if (process.env.RAZZLE_SENTRY_DSN || (typeof __SENTRY__ !== 'undefined' && __SENTRY__.SENTRY_DSN)) {
+    Sentry.withScope((scope) => {
+      scope.setTag('errorType', 'SSR');
+      scope.setContext('SSR Context', context);
+      if (context.url) {
+        scope.setTag('url', context.url);
+      }
+      if (context.statusCode) {
+        scope.setTag('statusCode', context.statusCode.toString());
+      }
+      Sentry.captureException(error);
+    });
+  }
+  return error;
+};
